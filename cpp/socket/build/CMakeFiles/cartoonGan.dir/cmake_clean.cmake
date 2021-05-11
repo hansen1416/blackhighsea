fae4a1abfac1205bbe9590fe426ade7a5940fba0
hansen1416 <badapplesweetie@gmail.com>
2021-05-11 20:41:21 +0800

    1

---
+++ cpp/socket/build/CMakeFiles/cartoonGan.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cartoonGan.dir/main.cpp.o"
+  "cartoonGan"
+  "cartoonGan.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cartoonGan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
