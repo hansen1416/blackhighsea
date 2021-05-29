1ee203b4947f17382d7c665d2af0ff16e38901db
hansen1416 <badapplesweetie@gmail.com>
2021-05-29 13:19:13 +0800

    1

--- api/py-flask/app/app/main.py
+++ api/py-flask/app/app/main.py
@@ -10,7 +10,7 @@ from flask_cors import CORS
 
 from stockname import StockNames
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/sharedvol')
 CORS(app)
 
 LSTM_RESULT_DIR = '/app/data/'
